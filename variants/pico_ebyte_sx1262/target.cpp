d9aa1b203b44e2810dbd24ba4e5c080798ee7876
Mikolaj8844 <146829222+Mikolaj8844@users.noreply.github.com>
2026-05-16 22:16:08 +0200

    Fix.

--- variants/pico_ebyte_sx1262/target.cpp
+++ variants/pico_ebyte_sx1262/target.cpp
@@ -5,14 +5,63 @@
 
 PicoEbyteSx1262Board board;
 
-RADIO_CLASS radio = new Module(P_LORA_NSS, P_LORA_DIO_1, P_LORA_RESET, P_LORA_BUSY, SPI1);
+RADIO_CLASS* radio;
 WRAPPER_CLASS radio_driver(radio, board);
 
 VolatileRTCClock fallback_clock;
 AutoDiscoverRTCClock rtc_clock(fallback_clock);
 SensorManager sensors;
 
+// bool radio_init() {
+//   rtc_clock.begin(Wire);
+
+//   SPI1.setSCK(P_LORA_SCLK);
+//   SPI1.setTX(P_LORA_MOSI);
+//   SPI1.setRX(P_LORA_MISO);
+
+
+//   SPI1.begin(P_LORA_SCLK, P_LORA_MISO, P_LORA_MOSI, P_LORA_NSS);
+
+//  // HARD RESET SX1262 (important)
+//   pinMode(P_LORA_RESET, OUTPUT);
+//   digitalWrite(P_LORA_RESET, LOW);
+//   delay(10);
+//   digitalWrite(P_LORA_RESET, HIGH);
+//   delay(50);
+
+//   pinMode(P_LORA_NSS, OUTPUT);
+//   digitalWrite(P_LORA_NSS, HIGH);
+//   // Serial.println("RESET PIN TOGGLING");
+
+
+// // pinMode(P_LORA_RESET, OUTPUT);
+// // digitalWrite(P_LORA_RESET, LOW);
+// // delay(20);
+// // digitalWrite(P_LORA_RESET, HIGH);
+// // delay(50);
+
+// // Serial.println("SPI READY, INIT RADIO...");
+// // bool ok = radio.std_init(NULL);
+
+// // Serial.print("RADIO INIT: ");
+// // Serial.println(ok);
+
+//   //passing NULL skips init of SPI
+//   // return radio.std_init(NULL);
+// // IMPORTANT: create module AFTER SPI init
+//   radio = new Module(P_LORA_NSS, P_LORA_DIO_1, P_LORA_RESET, P_LORA_BUSY, SPI1);
+
+//   bool ok = radio->std_init(NULL);
+
+//   Serial.print("SX1262 INIT: ");
+//   Serial.println(ok);
+
+//   return ok;
+// }
+
 bool radio_init() {
+  Serial.println("[RADIO] init start");
+
   rtc_clock.begin(Wire);
 
   SPI1.setSCK(P_LORA_SCLK);
@@ -20,12 +69,21 @@ bool radio_init() {
   SPI1.setRX(P_LORA_MISO);
 
   pinMode(P_LORA_NSS, OUTPUT);
+  pinMode(P_LORA_RESET, OUTPUT);
+  pinMode(P_LORA_BUSY, INPUT);
+
   digitalWrite(P_LORA_NSS, HIGH);
+  digitalWrite(P_LORA_RESET, HIGH);
 
-  SPI1.begin(false);
+  SPI1.begin();
 
-  //passing NULL skips init of SPI
-  return radio.std_init(NULL);
+  delay(50);
+
+  bool ok = radio.std_init(NULL);
+
+  Serial.printf("[RADIO] init result: %d\n", ok);
+
+  return ok;
 }
 
 uint32_t radio_get_rng_seed() {
